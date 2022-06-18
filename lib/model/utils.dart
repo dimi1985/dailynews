7bd5e62f55cfd6257a0bfeee8c7160c14ca4fa63
dimi1985 <xpilot88@hotmail.com>
2022-06-18 10:12:14 +0300

    Second Commit Replaced Files

---
+++ lib/model/utils.dart
@@ -0,0 +1,31 @@
+var listOfCountry = [
+  {"name": "Greece", "code": "gr"},
+  {"name": "Canada", "code": "ca"},
+  {"name": "USA", "code": "us"},
+  {"name": "MEXICO", "code": "mx"},
+  {"name": "New Zealand", "code": "nz"},
+  {"name": "Israel", "code": "il"},
+  {"name": "Japan", "code": "jp"},
+];
+
+var listOfCategory = [
+  {"name": "science", "code": "science"},
+  {"name": "business", "code": "business"},
+  {"name": "technology", "code": "technology"},
+  {"name": "health", "code": "health"},
+  {"name": "general", "code": "general"},
+  {"name": "entertainment", "code": "entertainment"},
+  {"name": "ALL", "code": null},
+];
+var listOfNewsChannel = [
+  {"name": "BBC News", "code": "bbc-news"},
+  {"name": "The Times of India", "code": "the-times-of-india"},
+  {"code": "politico", "name": "politico"},
+  {"code": "the-washington-post", "name": "The Washington Post"},
+  {"code": "reuters", "name": "reuters"},
+  {"code": "cnn", "name": "cnn"},
+  {"code": "nbc-news", "name": "nbc news"},
+  {"code": "the-hill", "name": "The Hill"},
+  {"code": "fox-news", "name": "Fox News"},
+  {"code": "fox-news", "name": "Fox News"},
+];
